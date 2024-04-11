53152df572f7378ee8bc3ea4f39cb268a80b272d
scarvalhofernando <scarvalho.fernando@outlook.com>
2024-04-11 17:51:06 -0300

    11-4-desafio-usando-jsonignoreproperties-e-jackson-mixin

--- src/main/java/com/algaworks/algafood/domain/model/mixin/RestauranteMixin.java
+++ src/main/java/com/algaworks/algafood/domain/model/mixin/RestauranteMixin.java
@@ -1,27 +1,19 @@
 package com.algaworks.algafood.domain.model.mixin;
 
-import com.algaworks.algafood.core.validation.Groups;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Endereco;
 import com.algaworks.algafood.domain.model.FormaPagamento;
 import com.algaworks.algafood.domain.model.Produto;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.*;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.groups.ConvertGroup;
-import javax.validation.groups.Default;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RestauranteMixin {
+public abstract class RestauranteMixin {
 
-    @JsonIgnoreProperties("nome")
+    @JsonIgnoreProperties(value = "nome", allowGetters = true)
     private Cozinha cozinha;
 
     @JsonIgnore
