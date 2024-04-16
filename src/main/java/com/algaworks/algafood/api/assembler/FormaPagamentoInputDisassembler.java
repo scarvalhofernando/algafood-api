bf8d3c5b94ca1489c38ee39902b18e55c412d136
scarvalhofernando <scarvalho.fernando@outlook.com>
2024-04-16 10:22:28 -0300

    12-5-desafio-implementado-os-endpoints-de-formas-de-pagamento-2-2

--- src/main/java/com/algaworks/algafood/api/assembler/FormaPagamentoInputDisassembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/FormaPagamentoInputDisassembler.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.assembler;
 
+import com.algaworks.algafood.api.model.input.FormaPagamentoInput;
 import com.algaworks.algafood.domain.model.FormaPagamento;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,5 +10,11 @@ public class FormaPagamentoInputDisassembler {
     @Autowired
     private ModelMapper modelMapper;
 
-    public FormaPagamento toDomainObject(FormaPagamentoInput formaPagamentoInput)
+    public FormaPagamento toDomainObject(FormaPagamentoInput formaPagamentoInput){
+        return modelMapper.map(formaPagamentoInput, FormaPagamento.class);
+    }
+
+    public void copyToDomainObject(FormaPagamentoInput formaPagamentoInput, FormaPagamento formaPagamento) {
+        modelMapper.map(formaPagamentoInput, formaPagamento);
+    }
 }
