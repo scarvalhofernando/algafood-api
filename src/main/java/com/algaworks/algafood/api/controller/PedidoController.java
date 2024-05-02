aa31bd65fbb57ecacc024749590c976d70d0ae37
scarvalhofernando <scarvalho.fernando@outlook.com>
2024-05-02 10:15:06 -0300

    13-2-limitando-os-campos-retornados-pela-api-com-jsonfilter

--- src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
@@ -12,8 +12,12 @@ import com.algaworks.algafood.domain.model.Pedido;
 import com.algaworks.algafood.domain.model.Usuario;
 import com.algaworks.algafood.domain.service.EmissaoPedidoService;
 import com.algaworks.algafood.repository.PedidoRepository;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -39,11 +43,26 @@ public class PedidoController {
     private PedidoInputDisassembler pedidoInputDisassembler;
 
     @GetMapping
-    public List<PedidoResumoModel> listar() {
-        List<Pedido> todosPedidos = pedidoRepository.findAll();
+    public MappingJacksonValue listar(@RequestParam(required = false) String campos) {
+        List<Pedido> pedidos = pedidoRepository.findAll();
+        List<PedidoResumoModel> pedidosModel = pedidoResumoModelAssembler.toCollectionModel(pedidos);
+        MappingJacksonValue pedidosWrapper = new MappingJacksonValue(pedidosModel);
+        SimpleFilterProvider filterProvider = new SimpleFilterProvider();
+        filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.serializeAll());
+        if(StringUtils.isNotBlank(campos)){
+            filterProvider.addFilter("pedoidoFilter", SimpleBeanPropertyFilter.filterOutAllExcept(campos.split(",")));
+        }
 
-        return pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
+        pedidosWrapper.setFilters(filterProvider);
+        return pedidosWrapper;
     }
+
+//    @GetMapping
+//    public List<PedidoResumoModel> listar() {
+//        List<Pedido> todosPedidos = pedidoRepository.findAll();
+//
+//        return pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
+//    }
 
     @GetMapping("/{codigoPedido}")
     public PedidoModel buscar(@PathVariable String codigoPedido) {
--- src/main/java/com/algaworks/algafood/api/model/PedidoResumoModel.java
+++ src/main/java/com/algaworks/algafood/api/model/PedidoResumoModel.java
@@ -1,16 +1,18 @@
 package com.algaworks.algafood.api.model;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
+@JsonFilter("pedidoFilter")
 @Setter
 @Getter
 public class PedidoResumoModel {
 
-    private Long id;
+    private String codigo;
     private BigDecimal subtotal;
     private BigDecimal taxaFrete;
     private BigDecimal valorTotal;
