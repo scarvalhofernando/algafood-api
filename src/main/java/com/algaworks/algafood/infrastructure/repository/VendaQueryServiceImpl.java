baf73325c699ec2778d9240569dade6f637979be
scarvalhofernando <scarvalho.fernando@outlook.com>
2024-05-07 09:44:35 -0300

    13-14-implementando-consulta-com-dados-agregados-de-vendas-diarias

--- src/main/java/com/algaworks/algafood/infrastructure/repository/VendaQueryServiceImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/VendaQueryServiceImpl.java
@@ -1,17 +1,38 @@
 package com.algaworks.algafood.infrastructure.repository;
 
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
+import com.algaworks.algafood.domain.model.Pedido;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 import com.algaworks.algafood.domain.service.VendaQueryService;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public class VendaQueryServiceImpl implements VendaQueryService {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @Override
     public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro){
-        return null;
+        var builder = manager.getCriteriaBuilder();
+        var query = builder.createQuery(VendaDiaria.class);
+        var root = query.from(Pedido.class);
+
+        var functionDateDataCriacao = builder.function("date", Date.class, root.get("dataCriacao"));
+
+        var selection = builder.construct(VendaDiaria.class,
+                functionDateDataCriacao,
+                builder.count(root.get("id")),
+                builder.sum(root.get("valorTotal")));
+
+        query.select(selection);
+        query.groupBy(functionDateDataCriacao);
+
+        return manager.createQuery(query).getResultList();
     }
 }
