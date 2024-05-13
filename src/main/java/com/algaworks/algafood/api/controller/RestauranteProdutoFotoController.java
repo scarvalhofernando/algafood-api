5c436959b4595eb36a20d2407acc134a3b0f83c5
scarvalhofernando <scarvalho.fernando@outlook.com>
2024-05-13 16:22:40 -0300

    implementando-upload-de-arquivo-com-multipart/form-data

---
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -0,0 +1,35 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.api.model.input.FotoProdutoInput;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.websocket.server.PathParam;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(value = "/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
+public class RestauranteProdutoFotoController {
+
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, FotoProdutoInput fotoProdutoInput){
+        var nomeArquivo = UUID.randomUUID().toString()
+                + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
+        var arquivoFoto = Path.of("C:/Users/scarv/Catalago", nomeArquivo);
+
+        System.out.println(fotoProdutoInput.getDescricao());
+        System.out.println(arquivoFoto);
+        System.out.println(fotoProdutoInput.getArquivo().getContentType());
+
+        try {
+            fotoProdutoInput.getArquivo().transferTo(arquivoFoto);
+        } catch (IllegalStateException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
