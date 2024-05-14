f9d3c2767d6fa2f4327ded22b3954026d95e24eb
scarvalhofernando <scarvalho.fernando@outlook.com>
2024-05-14 15:32:03 -0300

    14-7-excluindo-e-substituindo-cadastro-de-foto-de-produto

--- src/main/java/com/algaworks/algafood/infrastructure/repository/ProdutoRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/ProdutoRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.infrastructure.repository;
 import com.algaworks.algafood.domain.model.FotoProduto;
 import com.algaworks.algafood.repository.ProdutoRepositoryQueries;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -13,8 +14,15 @@ public class ProdutoRepositoryImpl implements ProdutoRepositoryQueries {
     @PersistenceContext
     private EntityManager manager;
 
+    @Transactional
     @Override
     public FotoProduto save(FotoProduto foto) {
         return manager.merge(foto);
+    }
+
+    @Transactional
+    @Override
+    public void delete(FotoProduto foto) {
+        manager.remove(foto);
     }
 }
