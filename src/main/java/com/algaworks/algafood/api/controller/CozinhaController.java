5a466fb8c8820bcd831a4e7c5f66880c27c39fe4
scarvalhofernando <scarvalho.fernando@outlook.com>
2024-07-02 09:12:04 -0300

    23-21-Method-Security-Restringindo-acesso-com-PreAuthorize-e-SpEL

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -36,6 +37,7 @@ public class CozinhaController {
     @Autowired
     private CozinhaInputDisassembler cozinhaInputDisassembler;
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping
     public Page<CozinhaDTO> listar(@PageableDefault(size = 10) Pageable pageable) {
         Page<Cozinha> cozinhasPage = cozinhaRepository.findAll(pageable);
@@ -49,6 +51,7 @@ public class CozinhaController {
         return cozinhasModelPage;
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/{cozinhaId}")
     public CozinhaDTO buscar(@PathVariable("cozinhaId") Long cozinhaId){
         Cozinha cozinha = cadastroCozinha.buscarOuFalhar(cozinhaId);
@@ -56,6 +59,7 @@ public class CozinhaController {
         return cozinhaModelAssembler.toDto(cozinha);
     }
 
+    @PreAuthorize("hasAuthority('EDITAR_COZINHAS')")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CozinhaDTO adicionar(@RequestBody @Valid CozinhaInput cozinhaInput){
@@ -65,6 +69,7 @@ public class CozinhaController {
         return cozinhaModelAssembler.toDto(cozinha);
     }
 
+    @PreAuthorize("hasAuthority('EDITAR_COZINHAS')")
     @PutMapping("/{cozinhaId}")
     public CozinhaDTO atualizar(@PathVariable Long cozinhaId,
                                              @RequestBody @Valid CozinhaInput cozinhaInput){
@@ -75,6 +80,7 @@ public class CozinhaController {
         return cozinhaModelAssembler.toDto(cozinhaAtual);
     }
 
+    @PreAuthorize("hasAuthority('EDITAR_COZINHAS')")
     @DeleteMapping("/{cozinhaId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void remover(@PathVariable Long cozinhaId){
