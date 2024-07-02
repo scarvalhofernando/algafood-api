3c541713ae70dcd545faae118a521dbb51efb420
scarvalhofernando <scarvalho.fernando@outlook.com>
2024-07-02 15:15:35 -0300

    23-26-Restringindo-acesso-a-endpoints-por-escopos-do-OAuth2

--- src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
+++ src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
@@ -10,13 +10,13 @@ import java.lang.annotation.Target;
 public @interface CheckSecurity {
 
     public @interface Cozinhas {
-        @PreAuthorize("hasAuthority('EDITAR_COZINHAS')")
+        @PreAuthorize("hasAuthority('SCOPE_WRITE') and hasAuthority('EDITAR_COZINHAS')")
         @Retention(RetentionPolicy.RUNTIME)
         @Target(ElementType.METHOD)
         public @interface PodeEditar {
         }
 
-        @PreAuthorize("isAuthenticated()")
+        @PreAuthorize("hasAuthority('SCOPE_READ') and isAuthenticated()")
         @Retention(RetentionPolicy.RUNTIME)
         @Target(ElementType.METHOD)
         public @interface PodeConsultar {
