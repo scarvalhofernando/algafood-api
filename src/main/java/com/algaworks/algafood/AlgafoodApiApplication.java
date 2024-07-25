9fc1c7b8f29263002143d3e1cdb838a6a3725353
scarvalhofernando <scarvalho.fernando@outlook.com>
2024-07-25 16:50:26 -0300

    23.46-externalizando-o-keystore-criando-um-protocolresolver-para-base64

--- src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
+++ src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood;
 
+import com.algaworks.algafood.core.io.Base64ProtocolResolver;
 import com.algaworks.algafood.infrastructure.CustomJpaRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,7 +16,10 @@ public class AlgafoodApiApplication {
 
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-		SpringApplication.run(AlgafoodApiApplication.class, args);
+//		SpringApplication.run(AlgafoodApiApplication.class, args);
+		var app = new SpringApplication(AlgafoodApiApplication.class);
+		app.addListeners(new Base64ProtocolResolver());
+		app.run(args);
 	}
 
 }
